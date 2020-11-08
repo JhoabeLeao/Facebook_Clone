c336883f3571e8bc2a2110f5d429f107462ec244
JhoabeLeao <jhoabe.leao@hotmail.com>
2020-11-07 23:19:51 -0300

    Project Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {FaFacebook, FaSearch, FaAlignJustify } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="headerLeft">
+          <div className="logo-fb">
+            <FaFacebook />
+          </div>
+          <div className="search-fb">
+            <FaSearch />
+          </div>
+          <div className="menu-fb">
+            <FaAlignJustify />
+          </div>
+
+        </div>
+        <div className="headerRight">
+           
+        </div>
+      </div>
+    
     </div>
   );
 }
