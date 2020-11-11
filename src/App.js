dba69680560093c116cda6bf83e2aa3a53253c87
JhoabeLeao <jhoabe.leao@hotmail.com>
2020-11-10 23:14:26 -0300

    Creation of stories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Header from './Header';
+import facebook from './resources/facebook_bg.png';
 
 function App() {
   return (
     <div className="App">
-        <Header />        
+        <Header />
+        <div className="stories">   
+          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+            <p>Jhoabe Leão</p>
+          </div>     
+          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+            <p>Jhoabe Leão</p>
+          </div>     
+          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+            <p>Jhoabe Leão</p>
+          </div>     
+          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+            <p>Jhoabe Leão</p>
+          </div>     
+          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+            <p>Jhoabe Leão</p>
+          </div>   
+        </div>    
     </div>
   );
 }
