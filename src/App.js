bffe5c24714581ddafcf544e031e989edb3104de
JhoabeLeao <jhoabe.leao@hotmail.com>
2020-11-11 21:34:36 -0300

    Stories

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 import Header from './Header';
-import facebook from './resources/facebook_bg.png';
+import Stories from './Stories';
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <div className="stories">   
-          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
-            <p>Jhoabe Leão</p>
-          </div>     
-          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
-            <p>Jhoabe Leão</p>
-          </div>     
-          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
-            <p>Jhoabe Leão</p>
-          </div>     
-          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
-            <p>Jhoabe Leão</p>
-          </div>     
-          <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
-            <p>Jhoabe Leão</p>
-          </div>   
-        </div>    
+        <Stories /> 
     </div>
   );
 }
---
+++ src/Stories.js
@@ -0,0 +1,26 @@
+
+ import facebook from './resources/facebook_bg.png';
+
+
+ export default function(){
+     return(
+         <div className="stories">   
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+             <p>Jhoabe Leão</p>
+           </div>     
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+             <p>Jhoabe Leão</p>
+           </div>     
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+             <p>Jhoabe Leão</p>
+           </div>     
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+             <p>Jhoabe Leão</p>
+           </div>     
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+             <p>Jhoabe Leão</p>
+           </div>   
+         </div> 
+     );
+ }
+ 
