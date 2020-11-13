67fb16a17b34b5955f3fdfc7bc2611e5c78ae637
JhoabeLeao <jhoabe.leao@hotmail.com>
2020-11-12 23:25:11 -0300

    Create a feed form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './Header';
 import Stories from './Stories';
+import facebook_bg from './resources/facebook_bg.png';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <Stories /> 
+        <div className="postarFeed">
+          <div className="feedForm">
+            <img src={facebook_bg} />
+            <input type="text" />
+          </div>
+        </div>  
     </div>
   );
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,23 +1,21 @@
-
- import facebook from './resources/facebook_bg.png';
-
+ import facebook_bg from './resources/facebook_bg.png';
 
  export default function(){
      return(
          <div className="stories">   
-           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook_bg})'}}>
              <p>Jhoabe Leão</p>
            </div>     
-           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook_bg})'}}>
              <p>Jhoabe Leão</p>
            </div>     
-           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook_bg})'}}>
              <p>Jhoabe Leão</p>
            </div>     
-           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook_bg})'}}>
              <p>Jhoabe Leão</p>
            </div>     
-           <div className="storiesCard" style={{backgroundImage:'url(${facebook})'}}>
+           <div className="storiesCard" style={{backgroundImage:'url(${facebook_bg})'}}>
              <p>Jhoabe Leão</p>
            </div>   
          </div> 
